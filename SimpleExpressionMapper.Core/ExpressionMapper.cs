f74088f49371465a0cc8219d06273bdf7709504c
Семенов Вадим Сергеевич <Semenov.Vadim_S@russianpost.ru>
2025-04-12 18:22:39 +0300

    Code refactor

--- SimpleExpressionMapper.Core/ExpressionMapper.cs
+++ SimpleExpressionMapper.Core/ExpressionMapper.cs
@@ -6,13 +6,18 @@ namespace SimpleExpressionMapper.Core;
 
 public interface IMapper
 {
+    //Мэппер простой, один метод для конвертирования и кэширования конвертируемых типов
     TDestination Map<TSource, TDestination>(TSource source); 
 }
 
 public class ExpressionMapper : IMapper
 {
+    //Словарь для хранения делегатов производящих мэппинг
     private readonly ConcurrentDictionary<TypesMapStorage, Delegate?> _delegatesStorage = new();
     
+    //Основной метод для вызова мэппинга.
+    //TSource - исходный тип для конвертации
+    //TDestination - тип в который будем конвертировать
     public TDestination Map<TSource, TDestination>(TSource source)
     {
        var creatorDelegate = GetCreatorDelegate<TSource, TDestination>();
@@ -23,10 +28,14 @@ public class ExpressionMapper : IMapper
        return creatorDelegate(source);
     }
 
+    //Метод, который возвращает делегат для мэппинга.
     private Func<TSource, TDestination>? GetCreatorDelegate<TSource, TDestination>()
     {
+        //Получаем ключ для словаря.
         var key = new TypesMapStorage(typeof(TSource), typeof(TDestination));
-
+        
+        //Если делегата еще нет, то он создается и сохраняется в словарь _delegatesStorage.
+        //Если есть нужный делегат в словаре, то просто возвращаем его.
         if (_delegatesStorage.TryGetValue(key, out var creatorDelegate))
         {
             return creatorDelegate as Func<TSource, TDestination>;
@@ -40,13 +49,18 @@ public class ExpressionMapper : IMapper
         }
     }
 
+    //Метод для компиляции делегата конвертации из дерева выражений
     private static Func<TSource, TDestination> CompileCreatorDelegate<TSource, TDestination>()
     {
+        //Извлекаем свойства из исходного типа(класса) и типа в который будем конвертировать.
+        //Проверяем, что свойство результирующего типа не только для чтения.  
         var sourceTypeProperties = typeof(TSource).GetProperties(BindingFlags.Instance|BindingFlags.Public);
         var destinationTypeProperties = typeof(TDestination).GetProperties(BindingFlags.Instance|BindingFlags.Public)
             .Where(p=> p.CanWrite)
             .ToList();
 
+        //Сопоставляем совпадающие имена свойства исходного и результирующего типа.
+        //И проверяем что тип свойства в обоих классах одинаковый (например int и int).
         var propertiesPair = sourceTypeProperties
             .Join(destinationTypeProperties,
                 s => s.Name,
@@ -55,15 +69,21 @@ public class ExpressionMapper : IMapper
             .Where(p => p.sourceProperty.PropertyType.IsAssignableTo(p.destinationProperty.PropertyType))
             .ToList();
          
+        //Выражение для создания параметра лямбды, по сути он будет компилироваться в такое лямбда выражение: (source) => ...
         var sourceTypeParameter = Expression.Parameter(typeof(TSource), "source");
         
+        //Выражение для присвоения значения свойств исходного типа свойствам результирующего типа. 
         var propertiesBindings = propertiesPair.Select(p=>
             Expression.Bind(p.destinationProperty, Expression.Property(sourceTypeParameter, p.sourceProperty)));
         
+        //Выражение для создания объекта результирующего типа, и назначения свойств 
         var createInstanceExpression  = Expression.MemberInit(Expression.New(typeof(TDestination)), propertiesBindings);
         
+        //Компиляция делегата для мэппинга
         return Expression.Lambda<Func<TSource, TDestination>>(createInstanceExpression, sourceTypeParameter).Compile();
     }
 }
 
+//А это у нас ключ для кэширования в словарь делегата. Он состоит из исходного и результирующего типа.
+//Как вы помните метод GetHashCode(и Equals) в record переопределяется автоматически.
 public record TypesMapStorage(Type SourceType, Type DestinationType);
--- SimpleExpressionMapper.Main/Program.cs
+++ SimpleExpressionMapper.Main/Program.cs
@@ -1,51 +1,43 @@
 ﻿using SimpleExpressionMapper.Core;
 
-namespace SimpleExpressionMapper.Main;
-
-class Program
+//Исходный объект класса
+var order = new Order
 {
-    static void Main(string[] args)
-    {
-        var order = new Order
-        {
-            OrderId = 1,
-            OrderName = "OrderName",
-            OrderDate = DateTime.Now
-        };
+	OrderId = 1,
+	OrderName = "OrderName",
+	OrderDate = DateTime.Now
+};
 
-        var mapper = new ExpressionMapper();
+var mapper = new ExpressionMapper();
 
-        var orderDto = mapper.Map<Order, OrderDto>(order);
-        var orderDto1 = mapper.Map<Order, OrderDto>(order);
-        var orderDto2 = mapper.Map<Order, OrderDto2>(order);
+var orderDto = mapper.Map<Order, OrderDto>(order);
+//Так как делегат уже создан, при втором вызове с одинаковыми типами,
+//он достанется из кэша (словаря)
+var orderDto1 = mapper.Map<Order, OrderDto>(order);
+var orderDto2 = mapper.Map<Order, OrderDto2>(order);
 
-        Console.Read();
-    }
+Console.Read();
 
-    public class Order
-    {
-        public int OrderId { get; set; }
+//Исходный тип
+public class Order
+{
+	public int OrderId { get; set; }
+	public string OrderName { get; set; } = null!;
+	public DateTime OrderDate { get; set; }
+}
 
-        public string OrderName { get; set; } = null!;
+//Результирующий тип
+public class OrderDto
+{
+	public int OrderId { get; set; }
+	public string OrderName { get; set; } = null!;
+	public DateTime OrderDate { get; set; }
+}
 
-        public DateTime OrderDate { get; set; }
-    }
-
-    public class OrderDto
-    {
-        public int OrderId { get; set; }
-
-        public string OrderName { get; set; } = null!;
-
-        public DateTime OrderDate { get; set; }
-    }
-
-    public class OrderDto2
-    {
-        public int OrderId { get; set; }
-
-        public string OrderName { get; set; } = null!;
-
-        public DateTime OrderDate { get; set; }
-    }
+//Еще один результирующий тип 
+public class OrderDto2
+{
+	public int OrderId { get; set; }
+	public string OrderName { get; set; } = null!;
+	public DateTime OrderDate { get; set; }
 }
